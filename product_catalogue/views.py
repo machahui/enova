e189244702f5bd9d7de05c548b3b73e81d020496
Miquias Achahui <miky1607@gmail.com>
2018-05-04 03:48:27 -0500

    pagination and search

--- product_catalogue/views.py
+++ product_catalogue/views.py
@@ -2,16 +2,25 @@ from django.http import Http404
 from .models import Product
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.db.models import Q
 
 
 def index(request):
     params = request.GET.get('name')
     if params:
-        products = Product.objects.filter(name__icontains=params)
+        products_list = Product.objects.filter(Q(name__icontains=params) | Q(description__icontains=params)).distinct()
     else:
-        products = Product.objects.all()
+        products_list = Product.objects.all()
 
+    paginator = Paginator(products_list, 25)  # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
 
     context = {'products': products}
     return render(request, 'product/index.html', context)
